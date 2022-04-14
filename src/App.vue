fb7940e78a66dfe6cb47ca216c9bbf51162e974a
yanghaoran <1064137887@qq.com>
2022-04-14 16:46:50 +0800

    关闭eslint检测，尝试使用自定义Tabbar组件（Vue3中可以直接用组件名书写html文本，即为<Tabbar/>而非视频中<tab-bar/>）

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-<div>hello world</div>
+  <div id="App">
+    <Tabbar/>
+  </div>
 </template>
 
 <script>
+import Tabbar from "./components/tabbar/Tabbar.vue"
 
 export default {
   name: 'App',
   components: {
+    Tabbar,
   }
 }
 </script>
 
 <style>
+
 </style>
